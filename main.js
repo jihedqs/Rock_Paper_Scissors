f20db4a700d6a4e97354bd47a1c6342230a5b218
jihed <jihedrezgani@gmail.com>
2023-09-08 12:10:36 +0100

    mod js

--- main.js
+++ main.js
@@ -1,57 +1,54 @@
 let arr = ["rock", "paper", "scissors"];
 function getComputerChoice() {
-    let index = Math.floor(Math.random() * 3);
-    let computerSelection = arr[index];
-    return computerSelection;
+  let index = Math.floor(Math.random() * 3);
+  let computerSelection = arr[index];
+  return computerSelection;
 }
-let score = 0
-let botScore = 0
+let score = 0;
+let botScore = 0;
 function game() {
-    for (let i = 0; i < 5; i++) {
-        function playRound(playerSelection, computerSelection) {
-            lowPlayer = playerSelection.toLowerCase()
-            if (lowPlayer == computerSelection) {
-                return "draw"
-            }
-            else if ((lowPlayer == 'rock' && computerSelection == 'scissors') ||
-                (lowPlayer == 'paper' && computerSelection == 'rock') ||
-                (lowPlayer == 'scissors' && computerSelection == 'paper')) {
-
-                score += 1
-                botScore -= 1
-                return `You win! ${playerSelection} beats ${computerSelection}`
-            }
-            else {
-                score -= 1
-                botScore += 1
-                return `You Lose! ${computerSelection} beats ${playerSelection}`
-            }
-
-        }
-
-        function conditionSeclect() {
-            let x = prompt("enter:")
-            while (x.toLowerCase() != 'rock' && x.toLowerCase() != 'scissors' && x.toLowerCase() != 'paper') {
-                x = prompt("enter again:")
-            }
-            return x
-        }
-
-        const playerSelection = conditionSeclect();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-
-
+  for (let i = 0; i < 5; i++) {
+    function playRound(playerSelection, computerSelection) {
+      lowPlayer = playerSelection.toLowerCase();
+      if (lowPlayer == computerSelection) {
+        return "draw";
+      } else if (
+        (lowPlayer == "rock" && computerSelection == "scissors") ||
+        (lowPlayer == "paper" && computerSelection == "rock") ||
+        (lowPlayer == "scissors" && computerSelection == "paper")
+      ) {
+        score += 1;
+        botScore -= 1;
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+      } else {
+        score -= 1;
+        botScore += 1;
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+      }
     }
-    if (score > botScore) {
-        console.log("you won " + score)
+
+    function conditionSeclect() {
+      do {
+        let x = prompt("enter:");
+      } while (
+        x.toLowerCase() != "rock" &&
+        x.toLowerCase() != "scissors" &&
+        x.toLowerCase() != "paper"
+      );
+      return x;
     }
-    else if (score == botScore) {
-        console.log("Draw " + score)
-    }
-    else {
-        console.log("you lose " + score)
-    }
+
+    const playerSelection = conditionSeclect();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+  if (score > botScore) {
+    console.log("you won " + score);
+  } else if (score == botScore) {
+    console.log("Draw " + score);
+  } else {
+    console.log("you lose " + score);
+  }
 }
 
-game()
+game();
