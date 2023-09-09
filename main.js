aa7f7358ce6976077e5d00a0bcdd0ff368e0f230
jihed <jihedrezgani@gmail.com>
2023-09-09 21:58:01 +0100

    clean code

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
 let arr = ["rock", "paper", "scissors"];
+let score = 0;
+let botScore = 0;
+
 function getComputerChoice() {
   let index = Math.floor(Math.random() * 3);
-  let computerSelection = arr[index];
-  return computerSelection;
+  return arr[index];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    console.log("draw");
+  } else if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  ) {
+    score += 1;
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    console.log(score + " / " + botScore);
+  } else {
+    botScore += 1;
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    console.log(score + " / " + botScore);
+  }
 }
 
 function game() {
-  let score = 0;
-  let botScore = 0;
   while (score != 5 && botScore != 5) {
-    function playRound(playerSelection, computerSelection) {
-      lowPlayer = playerSelection.toLowerCase();
-      if (lowPlayer == computerSelection) {
-        console.log("draw");
-      } else if (
-        (lowPlayer == "rock" && computerSelection == "scissors") ||
-        (lowPlayer == "paper" && computerSelection == "rock") ||
-        (lowPlayer == "scissors" && computerSelection == "paper")
-      ) {
-        score += 1;
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        console.log(score + " / " + botScore);
-      } else {
-        botScore += 1;
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-        console.log(score + " / " + botScore);
-      }
-    }
-
     const playerSelection = prompt("enter:");
     const computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
